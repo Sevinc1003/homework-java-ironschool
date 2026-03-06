2fbf794963cc92ed978ebccfc8b5ce7b0a238306
Sevinc <sevincquliyeva000@gmail.com>
2026-03-06 15:55:36 +0400

    maps added

---
+++ ironschool/src/main/java/az/ironschool/ironschool/services/TeacherService.java
@@ -0,0 +1,12 @@
+package az.ironschool.ironschool.services;
+
+import java.util.Map;
+
+import az.ironschool.ironschool.entities.Student;
+import az.ironschool.ironschool.entities.Teacher;
+
+public class TeacherService {
+    private static Map<String, Teacher> teachers;
+
+
+}
